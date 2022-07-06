9c83b55129d36b7b4607642bdd46071dd403f2fa
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-06 19:10:12 +0000

    add stdin

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
-const connect  = require('./client');
+const connect = require('./client');
 console.log("Connecting ...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const stdin = setupInput();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+};
+
+stdin.on("data", (key) => handleUserInput(key));
